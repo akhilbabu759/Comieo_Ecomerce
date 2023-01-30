ea77abed730378271942411799dfdfe3b5805cdd
akhil babu <85557134+akhilbabu759@users.noreply.github.com>
2023-01-31 04:18:54 +0530

    home card background color is changed to dark

--- lib/screen/home/view/widget/cardView/home_card.dart
+++ lib/screen/home/view/widget/cardView/home_card.dart
@@ -1,41 +1,54 @@
 import 'package:ecomerce/common/api_baseurl.dart';
 import 'package:ecomerce/core/constent.dart';
+import 'package:ecomerce/core/text_style.dart';
 
 import 'package:flutter/material.dart';
 
 class HomeCard extends StatelessWidget {
-  const HomeCard({Key? key, required this.imag, required this.text}) : super(key: key);
+  const HomeCard({Key? key, required this.imag, required this.text})
+      : super(key: key);
   final String text;
   final String imag;
-  
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      borderOnForeground: true,
-    elevation: 3,
-      child: Column(
-        children: [
-          Container(height: 27,
-            width: 140,
-            color: Colors.blueGrey[50],
-            child: Center(
-              child: Text(
-                text,
-                style: const TextStyle(color: colorblack),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 140,
-            width: 140,
-            child: Image.network(
-              '${ApiBaseUrl().baseurl}/category/$imag',fit: BoxFit.scaleDown,
-              // height: 100,
-              // width: 150,
-            ),
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 8,
           )
         ],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Card(
+        borderOnForeground: true,
+        elevation: 3,
+        child: Column(
+          children: [
+            Container(
+              height: 27,
+              width: 140,
+              color: Colors.blueGrey[50],
+              child: Center(
+                child: Text(
+                  text,
+                  style: const TextStyle(color: colorblack),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 140,
+              width: 140,
+              child: Image.network(
+                '${ApiBaseUrl().baseurl}/category/$imag', fit: BoxFit.scaleDown,
+                // height: 100,
+                // width: 150,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
