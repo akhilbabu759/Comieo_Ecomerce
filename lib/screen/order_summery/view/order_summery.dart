5d0aa2ab5a2c2f1d8cbfd1008d4a24455d421741
akhil babu <85557134+akhilbabu759@users.noreply.github.com>
2023-02-08 13:59:04 +0530

    order summery page address design finished

--- lib/screen/order_summery/view/order_summery.dart
+++ lib/screen/order_summery/view/order_summery.dart
@@ -24,27 +24,68 @@ class OrderSummery extends StatelessWidget {
             style: TextStyle(color: colorblack, fontSize: 20),
           )),
       body: SafeArea(
-          child: Column(
-        children: [
-          Row(
-            children: [
-              textFieldGap,
-              const Text('Deliver to:'),
-              ElevatedButton(
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                          const Color.fromARGB(255, 255, 255, 255)),
-                      elevation: MaterialStateProperty.all(0),
-                      side: MaterialStateProperty.all(
-                          const BorderSide(color: colorblack))),
-                  onPressed: () {},
-                  child: const Text(
-                    'Change',
-                    style: TextStyle(color: colorblack),
-                  ))
-            ],
-          )
-        ],
+          child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            textFieldGap,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text('Deliver to:'),
+                ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            const Color.fromARGB(255, 255, 255, 255)),
+                        elevation: MaterialStateProperty.all(0),
+                        side: MaterialStateProperty.all(
+                            const BorderSide(color: colorblack))),
+                    onPressed: () {},
+                    child: const Text(
+                      'Change',
+                      style: TextStyle(color: colorblack),
+                    ))
+              ],
+            ),
+            Container(
+              color: Colors.blueGrey[50],
+              child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Text('Shameer',style: TextStyle(fontSize: 24),),
+                        Card(
+                      child: Text('Home'),
+                     
+                    ),
+                      ],
+                    ),
+                    
+                     Row(children: [
+                      IconButton(onPressed: () {
+                        
+                      }, icon: Icon(Icons.edit)),
+                      IconButton(onPressed: () {
+                        
+                      }, icon: Icon(Icons.delete))
+
+                     ],)
+                  ],
+                ),
+                
+                const Text('loppo'),
+                const Text('PIN: 7894848,fjffk'),
+                const Text('gjgjgj'),
+                const SizedBox(height: 10,),
+                const Text('8909808797'),
+                const SizedBox(height: 10,),
+
+              ]),
+            )
+          ],
+        ),
       )),
     );
   }
---
+++ lib/screen/order_summery/view/widget/product_deatails.dart
@@ -0,0 +1,204 @@
+import 'package:ecomerce/common/api_baseurl.dart';
+import 'package:ecomerce/core/constent.dart';
+import 'package:ecomerce/screen/cart/controller/cart_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
+import 'package:get/get.dart';
+
+class OneProduct extends StatelessWidget {
+     OneProduct({Key? key, required this.index}) : super(key: key);
+   int   index;
+
+  @override
+  Widget build(BuildContext context) {
+    final cartcontrol = Get.put(CartController());
+    return Card(
+      child: Column(children: [
+        Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Image.network(
+            '${ApiBaseUrl().baseurl}/products/${cartcontrol.cartList!.products[index].product.image[0]}',
+            height: 150,
+            width: 150,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: Get.size.width * 0.440),
+                child: IconButton(
+                    onPressed: () {
+                      Get.defaultDialog(
+                          title: 'Remove Item',
+                          middleText: 'Are you sure want to remove this item?',
+                          onConfirm: () {
+                            cartcontrol.removeCart(cartcontrol
+                                .cartList!.products[index].product.id);
+                            Get.back();
+                          },
+                          textConfirm: 'Yes',
+                          textCancel: 'No');
+                    },
+                    icon: const Icon(Icons.dangerous)),
+              ),
+              Text(
+                cartcontrol.cartList!.products[index].product.name,
+                style: const TextStyle(
+                  fontSize: 21,
+                ),
+                maxLines: 2,
+              ),
+              textFieldGap,
+              RatingBar.builder(
+                          initialRating: double.parse(
+                               cartcontrol.cartList!.products[index].product.rating
+                              
+                              ),
+                          itemSize: 15,
+                          minRating: 1,
+                          direction: Axis.horizontal,
+                          allowHalfRating: true,
+                          ignoreGestures: true,
+                          itemBuilder: (context, _) => const Icon(
+                            Icons.star,
+                            color: Colors.amber,
+                          ),
+                          onRatingUpdate: (startRating) {
+                           
+                          },
+                        ),
+              textFieldGap,
+              RichText(
+                  text: TextSpan(children: [
+                TextSpan(
+                    text:
+                        '₹${cartcontrol.cartList!.products[index].product.price}',
+                    style: const TextStyle(
+                        decoration: TextDecoration.lineThrough,
+                        color: Color.fromARGB(255, 112, 114, 115),
+                        fontSize: 15)),
+                TextSpan(
+                    text:
+                        ' ₹${cartcontrol.cartList!.products[index].product.offer}',
+                    style: const TextStyle(color: Colors.black, fontSize: 20)),
+                TextSpan(
+                    text:
+                        ' ${cartcontrol.cartList!.products[index].product.discountPrice}% off',
+                    style: const TextStyle(
+                      color: Colors.green,
+                      fontSize: 20,
+                    ))
+              ])),
+              textFieldGap,
+              RichText(
+                  text: const TextSpan(children: [
+                TextSpan(
+                    text: ' Delivery in 4 days |',
+                    style: TextStyle(
+                        color: Color.fromARGB(
+                          255,
+                          112,
+                          114,
+                          115,
+                        ),
+                        fontSize: 12)),
+                TextSpan(
+                    text: ' Free delivery',
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 12,
+                    )),
+              ])),
+            ],
+          )
+        ]),
+        Padding(
+          padding: const EdgeInsets.only(right: 87),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 16.0),
+                child: Card(
+                  elevation: 3,
+                  child: Row(
+                    children: [
+                      Card(
+                        elevation: 3,
+                        child: ColoredBox(
+                          color: Colors.grey,
+                          child: GestureDetector(
+                              child: const Icon(Icons.add, size: 30),
+                              onTap: () {
+                                cartcontrol.incrementDecrementQty(
+                                    1,
+                                    cartcontrol
+                                        .cartList!.products[index].product.id,
+                                    cartcontrol.cartList!.products[index].qty,
+                                    cartcontrol
+                                        .cartList!.products[index].product.size[0]
+                                        .toString());
+                              }
+                              // cartcontrol.adding(),
+                              ),
+                        ),
+                      ),
+                      GetBuilder<CartController>(
+                        builder: (controller) {
+                          return SizedBox(
+                              width: 20,
+                              height: 24,
+                              child: Center(
+                                child: Text(
+                                  cartcontrol.cartList!.products[index].qty
+                                      .toString(),
+                                  style: const TextStyle(fontSize: 17),
+                                ),
+                              )
+                              // cartcontrol.value.toString()),
+                              );
+                        },
+                      ),
+                      Card(
+                        elevation: 3,
+                        child: ColoredBox(
+                          color: Colors.grey,
+                          child: GestureDetector(
+                              child: const Icon(Icons.remove, size: 30),
+                              onTap: () {
+                                cartcontrol.incrementDecrementQty(
+                                    -1,
+                                    cartcontrol
+                                        .cartList!.products[index].product.id,
+                                    cartcontrol.cartList!.products[index].qty,
+                                    cartcontrol
+                                        .cartList!.products[index].product.size[0]
+                                        .toString());
+                              }
+                              //  cartcontrol.deleting(),
+                              ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const Card(
+                elevation: 3,
+                child: SizedBox(
+                  height: 27,
+                  child: Center(
+                      child: Text(
+                    '  See more like this  ',
+                    style: TextStyle(fontSize: 13),
+                  )),
+                ),
+              )
+            ],
+          ),
+        ),
+        textFieldGap
+      ]),
+    );
+  }
+}
